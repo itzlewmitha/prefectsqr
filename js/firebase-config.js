e0b0a838eac0a8c6f9c2f6ee805a2e8b3935b2c2
Lewmitha Kithuldeniya <studioslewmithas@gmail.com>
2025-10-05 14:08:41 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,10 @@
 // js/firebase-config.js
+
+// Import Firebase modules properly
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBLN0tDCLOciir5FgcmUuhTWfJbK6_xqFk",
   authDomain: "prefectsqr.firebaseapp.com",
@@ -7,8 +13,11 @@ const firebaseConfig = {
   messagingSenderId: "157605240235",
   appId: "1:157605240235:web:0d2770b9a65669969006ee"
 };
-
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const auth = firebase.auth();
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Make them globally available
+window.db = db;
+window.auth = auth;
